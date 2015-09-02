c5f9f23505ce46fbd5c229b74d93271e61ab6630
Alex S <alexey.shchelokov@gmail.com>
2015-09-02 12:05:16 -0400

    test: make transaction bundle

--- app.js
+++ app.js
@@ -8,21 +8,34 @@
 "use strict";
 
 var fs = require('fs');
+var _ = require('lodash');
 
 var bbcms = require("./index");
+
+var makeTransactionalBundle = function (bundle, base, patientId) {
+    _.each(bundle.entry, function (value) {
+        value.transaction = {
+            'method': (value.resource.resourceType === 'Patient') ? 'PUT' : 'POST',
+            'url': (value.resource.resourceType === 'Patient') ? 'Patient/' + patientId : value.resource.resourceType
+        };
+        value.base = base;
+    });
+    bundle.type = 'transaction';
+    return bundle;
+};
 
 console.time('--> CcdaParserStream');
 
 var request = require('request');
-var istream = request.get('https://raw.githubusercontent.com/chb/sample_ccdas/master/Vitera/Vitera_CCDA_SMART_Sample.xml');
+//var istream = request.get('https://raw.githubusercontent.com/chb/sample_ccdas/master/Vitera/Vitera_CCDA_SMART_Sample.xml');
 
-//var istream = fs.createReadStream(__dirname + '/test/artifacts/bluebutton-01-original.xml', 'utf-8');
+var istream = fs.createReadStream(__dirname + '/test/artifacts/bluebutton-01-original.xml', 'utf-8');
 
 istream
-    .pipe(new bbcms.CcdaParserStream())
+    .pipe(new bbcms.CcdaParserStream("test"))
     .on('data', function (data) {
 
-        console.log(JSON.stringify(data, null, '  '));
+        console.log(JSON.stringify(makeTransactionalBundle(data,"http://localhost:8080/fhir","test"), null, '  '));
     })
     .on('finish', function () {
         console.timeEnd('--> CcdaParserStream');
