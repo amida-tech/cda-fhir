9371686e359aa8b3b483d4cc56499ac15265c14f
Alex S <alexey.shchelokov@gmail.com>
2015-10-01 16:33:13 -0400

    fix: some structire fix

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 "use strict";
 
 var fs = require('fs');
+var http = require('http');
 var _ = require('lodash');
 
 var bbcms = require("./index");
@@ -34,11 +35,91 @@ var istream = fs.createReadStream(__dirname + '/test/artifacts/bluebutton-01-ori
 istream
     .pipe(new bbcms.CcdaParserStream("test"))
     .on('data', function (data) {
+        var bundle = JSON.stringify(makeTransactionalBundle(data), null, '  ');
+        console.log(bundle);
+        var req = http.request({
+                hostname: 'localhost',
+                port: 8080,
+                path: '/fhir/baseDstu2',
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Content-Length': bundle.length
+                }
+            },
+            function (res) {
+                var response = '';
+                console.log('STATUS: ' + res.statusCode);
+                console.log('HEADERS: ' + JSON.stringify(res.headers));
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    console.log('BODY: ' + chunk);
+                    response = response + chunk;
+                });
+                res.on('end', function () {
+                    console.log('No more data in response.');
+                    var zipped = _.zip(data.entry, JSON.parse(response).entry);
+                    console.log(zipped);
 
-        console.log(JSON.stringify(makeTransactionalBundle(data, "http://localhost:8080/fhir", "test"), null, '  '));
+                    zipped.forEach(function (element) {
+                        var result2 = '';
+                        var req = http.request({
+                            hostname: 'localhost',
+                            port: 8080,
+                            path: '/fhir/baseDstu2/' + element[1].response.location,
+                            method: 'GET',
+                        }, function (res) {
+                            res.setEncoding('utf8');
+                            res.on('data', function (chunk) {
+                                //console.log('BODY2: ' + chunk);
+                                result2 = result2 + chunk;
+                            })
+                            .on('end', function() {
+                                var resource = JSON.parse(result2);
+                                var comparator = function (l, r, propn) {
+                                    if (_.isNumber(l)) {
+                                        return;
+                                    }
+                                    if (_.isString(l)) {
+                                        return;
+                                    }
+                                    if (_.isBoolean(l)) {
+                                        return;
+                                    }
+                                    if (_.isArray(l) && _.isArray(r)) {
+                                        if (l.length !== r.length) {
+                                            console.log('!-------- Array length differ %s', propn);
+                                        }
+                                        for (var i = 0; i < l.lenght && i < r.length; i++) {
+                                            comparator(l[i], r[i], propn);
+                                        }
+                                    }
+
+                                    for (var prop in l) {
+                                        if (l.hasOwnProperty(prop)) {
+                                            if (!r.hasOwnProperty(prop)) {
+                                                console.log('!-------- Missed prop ' + prop);
+                                            } else {
+                                                comparator(l[prop], r[prop], prop);
+                                            }
+                                        }
+                                    }
+                                };
+                                console.log('compare:\n %j \n %j', element[0].resource, resource);
+                                comparator(element[0].resource, resource);
+                            });
+                        });
+                        req.end();
+                    });
+                    //process.exit();
+                });
+            });
+        req.write(bundle);
+        req.end();
     })
     .on('finish', function () {
-        console.timeEnd('--> CcdaParserStream');
+
     })
     .on('error', function (error) {
         console.log(error);
